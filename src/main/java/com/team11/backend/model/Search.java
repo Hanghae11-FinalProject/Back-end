4bcfc5b14ad8c0fcf466855775ceda8b27754364
woojin126 <woojin126@naver.com>
2021-12-23 15:25:02 +0900

    add Search Entity

---
+++ src/main/java/com/team11/backend/model/Search.java
@@ -0,0 +1,25 @@
+package com.team11.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Search {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId")
+    private User user;
+
+    private String keyword;
+
+}
