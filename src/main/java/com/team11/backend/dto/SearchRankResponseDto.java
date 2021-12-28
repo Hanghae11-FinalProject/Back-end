1966fe957fe89ace110990a24bd51f1806a5d653
woojin126 <woojin126@naver.com>
2021-12-28 22:17:29 +0900

    SearchController 가장 많이 검색된 10개 단어 api

---
+++ src/main/java/com/team11/backend/dto/SearchRankResponseDto.java
@@ -0,0 +1,16 @@
+package com.team11.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SearchRankResponseDto {
+    List<String> SearchRankList;
+}
