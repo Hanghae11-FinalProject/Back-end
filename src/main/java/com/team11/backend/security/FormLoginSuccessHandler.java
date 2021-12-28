29bd7dc13e93eb960b177ebd9120b8074f9806c8
woojin126 <woojin126@naver.com>
2021-12-28 17:07:31 +0900

    로그인 성공시 헤더에 닉네임, 프로필 이미지 url 같이전송

--- src/main/java/com/team11/backend/security/FormLoginSuccessHandler.java
+++ src/main/java/com/team11/backend/security/FormLoginSuccessHandler.java
@@ -1,6 +1,7 @@
 package com.team11.backend.security;
 
 import com.team11.backend.security.jwtutil.JwtUtil;
+import com.team11.backend.security.oauth2.service.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
@@ -8,6 +9,8 @@ import org.springframework.stereotype.Component;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 
 @Component
@@ -16,13 +19,21 @@ public class FormLoginSuccessHandler extends SavedRequestAwareAuthenticationSucc
     private final JwtUtil jwtUtil;
     public static final String AUTH_HEADER = "Authorization";
     public static final String TOKEN_TYPE = "Bearer";
-
+    public static final String NICKNAME = "Nickname";
+    public static final String IMAGEURL = "profileImage";
     // ** 5. FormLogin 성공 및 JWT 토큰 생성 **
     @Override
     public void onAuthenticationSuccess(final HttpServletRequest request, final HttpServletResponse response,
                                         final Authentication authentication) throws IOException {
+        final CustomUserDetails userDetails = ((CustomUserDetails) authentication.getPrincipal());
+        byte[] bytes = userDetails.getUser().getNickname().getBytes(StandardCharsets.UTF_8);
+        byte[] bytes1 = userDetails.getUser().getProfileImg().getBytes(StandardCharsets.UTF_8);
         // Token 생성
         String token = jwtUtil.createToken(authentication);
+        String encodedNickname = Base64.getEncoder().encodeToString(bytes);
+        String encodedImageUrl = Base64.getEncoder().encodeToString(bytes1);
+        response.addHeader(NICKNAME, encodedNickname);
+        response.addHeader(IMAGEURL, encodedImageUrl);
         response.addHeader(AUTH_HEADER, TOKEN_TYPE + " " + token);
     }
 
