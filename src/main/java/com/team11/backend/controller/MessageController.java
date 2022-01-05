f7fb7a5dfe0767f54ece71a08dbfcd799e944857
woojin126 <woojin126@naver.com>
2022-01-05 21:53:12 +0900

    테스트

--- src/main/java/com/team11/backend/controller/MessageController.java
+++ src/main/java/com/team11/backend/controller/MessageController.java
@@ -16,7 +16,6 @@ public class MessageController {
     private final UserRepository userRepository;
     private final RoomRepository roomRepository;
 
-    //test
     //pub/api/message 클라이언트 요청으로 메세지 발행
     @MessageMapping("/message")
     public void message(MessageDto messageDto){
