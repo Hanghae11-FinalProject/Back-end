8288c923424196b93a7e602976173b0e8ab7b8d8
woojin126 <woojin126@naver.com>
2022-01-12 04:45:39 +0900

    무중단배포 테스트

--- src/main/java/com/team11/backend/BackendApplication.java
+++ src/main/java/com/team11/backend/BackendApplication.java
@@ -2,15 +2,19 @@ package com.team11.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class BackendApplication {
-
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "/home/ec2-user/app/config/springboot-webservice/real-application.yml";
     public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
-
+        //SpringApplication.run(BackendApplication.class, args);
+        new SpringApplicationBuilder(BackendApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
 }
 
