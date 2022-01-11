b012b878337ae1f5b2b97056de0d63945f677811
woojin126 <woojin126@naver.com>
2022-01-11 13:34:30 +0900

    test

--- src/main/java/com/team11/backend/BackendApplication.java
+++ src/main/java/com/team11/backend/BackendApplication.java
@@ -1,6 +1,5 @@
 package com.team11.backend;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -8,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class BackendApplication {
-    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+    public static final String APPLICATION_LOCATIONS = "Dspring.profiles.active="
             + "/home/ubuntu/yarn/application.yml,"
             + "/home/ec2-user/app/config/springboot-webservice/real-application.yml";
     public static void main(String[] args) {
