95ba560d1b2e146ddab9c3cb6da01be5ed1627b1
woojin126 <woojin126@naver.com>
2022-01-12 14:49:48 +0900

    test 무중단

--- src/main/java/com/team11/backend/controller/ProfileController.java
+++ src/main/java/com/team11/backend/controller/ProfileController.java
@@ -4,15 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.Arrays;
-import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 public class ProfileController {
 
-    private Environment env;
+    private final Environment env;
 
     @GetMapping("/profile")
     public String getProfile(){
