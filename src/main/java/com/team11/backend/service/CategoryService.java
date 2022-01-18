3c38fb8e998cb8c1692b13203ded84bb530428ec
woojin126 <woojin126@naver.com>
2022-01-18 21:11:41 +0900

    test

--- src/main/java/com/team11/backend/service/CategoryService.java
+++ src/main/java/com/team11/backend/service/CategoryService.java
@@ -22,7 +22,7 @@ public class CategoryService {
 
     private final CategoryRepository categoryRepository;
 
-    private final RedisTemplate<String,List<CategoryQueryDto>> redisTemplateList;
+   // private final RedisTemplate<String,List<CategoryQueryDto>> redisTemplateList;
 
     @Transactional
     public Page<CategoryQueryDto> categoryFilter(CategoryDto.RequestDto categoryRequestDto, Pageable pageable) {
@@ -39,7 +39,7 @@ public class CategoryService {
         postList.forEach(key -> key.setImages(imageIdMap.get(key.getPostId())));
         postList.forEach(key -> key.setBookMarks(bookMarkInUserIdMap.get(key.getPostId())));
 
-        redisTemplateList.opsForList().leftPush("category"+pageable.getPageNumber(), postList.stream().collect(Collectors.toList()));
+       // redisTemplateList.opsForList().leftPush("category"+pageable.getPageNumber(), postList.stream().collect(Collectors.toList()));
         return postList;
     }
 }
